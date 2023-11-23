31dbaca42129aa1f25d6ba5a0a1f17ee7d12f059
njjnjjnjj <njj1108@outlook.com>
2023-11-23 17:27:29 +0800

    feat: 声明与定义主页路由及其组件

---
+++ blog-view/client/src/views/IndexView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <p>
+    Edit <code>src/views/IndexView.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<style scoped>
+
+</style>
