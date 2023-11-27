973fb6209f4d11a460f916ba3f5e5e0f57d6ec28
njjnjjnjj <njj1108@outlook.com>
2023-11-27 18:20:48 +0800

    feat: 修改网页 title 与 icon

--- blog-view/client/src/components/BlogMusicShare.vue
+++ blog-view/client/src/components/BlogMusicShare.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
-
-import {VideoPlay} from "@element-plus/icons-vue";
+import Jenny from "@/assets/Jenny.mp3";
 </script>
 
 <template>
   <el-card>
     <template #header>最近在听</template>
-    <p><i>好不容易——告五人</i></p>
-    <audio src="http://music.163.com/#/song?id=1902251868" controls></audio>
+    <p style="text-align: center"><i>好不容易——告五人</i></p>
+    <audio :src="Jenny" controls style="width: 100%"></audio>
   </el-card>
 </template>
 
